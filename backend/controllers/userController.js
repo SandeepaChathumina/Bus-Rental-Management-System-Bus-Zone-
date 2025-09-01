64fe3e6adcf424b32e257c0618501e398f7d826d
SandeepaChathumina <sandeepachathuminaofficial@gmail.com>
2025-09-01 11:58:47 +0530

    fixed errors

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -100,10 +100,6 @@ const loginUser = async (req, res) => {
 const getUsers = async (req, res) => {
   try {
     const users = await User.find({}).select('-password');
-
-const getUsers = async (req, res) => {
-  try {
-    const users = await User.find({}).select('-password');
     
     const usersWithProfiles = await Promise.all(users.map(async (user) => {
       const userObj = user.toObject();
