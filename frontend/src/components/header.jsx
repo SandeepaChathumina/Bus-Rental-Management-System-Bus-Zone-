cb66bb36d9d165c654d9d8c3b24103831a20eb67
SandeepaChathumina <sandeepachathuminaofficial@gmail.com>
2025-09-02 05:55:16 +0530

    setup talwind css

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return(
+        <header className="w-full bg-blue-500 h-[50px] text-white">
+            <h1>My store</h1>
+            
+        </header>
+    )
+}
